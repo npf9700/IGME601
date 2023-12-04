8269f42c158374340ae8efc148b4d911c926a96a
npf9700 <70346282+npf9700@users.noreply.github.com>
2023-12-03 23:48:59 -0500

    Bug Riddance
    This commit allows the game to be playtest ready so that the game loop is complete. Not all of the features that are in the project are included due to bugs

---
+++ IGME601/Assets/Scripts/CameraTransition.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransition : MonoBehaviour
+{
+    [SerializeField]
+    private Transform spawnSpot;
+
+    public Transform SpawnSpot
+    {
+        get { return spawnSpot; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
